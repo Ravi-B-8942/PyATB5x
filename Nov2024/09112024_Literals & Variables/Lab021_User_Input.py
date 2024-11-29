331c18a88ca47f3e8d1f668073b745d70e449e52
RaviB <baburavi.bellary90@gmail.com>
2024-11-29 12:55:07 +0530

    09112024 committed

---
+++ Nov2024/09112024_Literals & Variables/Lab021_User_Input.py
@@ -0,0 +1,3 @@
+# input
+Name = input("Enter Your Name")
+print("Your Name is",Name)
---
+++ Nov2024/09112024_Literals & Variables/Lab022_Sum of 2 NUmbers.py
@@ -0,0 +1,30 @@
+# Num1 = input("Enter th number 1")
+# Num2 = input("Enter the number 2")
+
+# sum = Num1 + Num2
+# sub = Num1 - Num2
+# mul = Num1 * Num2
+# div = Num1 / Num2
+
+# print("sum of 2 No's is :", sum)
+# print("Sub of 2 No's is :", sub)
+# print("Mul of 2 No's is :", mul)
+# print("Div of 2 No's is :", div)
+
+# Result is getting failed, because input will be in str format so need to convert it for Int format, later we can execute
+
+
+Num1 = int(input("Enter th number 1"))
+Num2 = int(input("Enter the number 2"))
+
+sum = Num1 + Num2
+sub = Num1 - Num2
+mul = Num1 * Num2
+div = Num1 / Num2
+
+print("sum of 2 No's is :", sum)
+print("Sub of 2 No's is :", sub)
+print("Mul of 2 No's is :", mul)
+print("Div of 2 No's is :", div)
+
+
