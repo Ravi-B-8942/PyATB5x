175b23570a75ace197321ebada41c74364fa7242
RaviB <baburavi.bellary90@gmail.com>
2024-11-29 13:43:39 +0530

    09112024 committed.

---
+++ Nov2024/09112024_Literals & Variables/Lab029_Task-09112024.py
@@ -0,0 +1,15 @@
+# take input of 3 number & perform Sum, Sub, Mul, Div
+
+Num1 = int(input("Enter the No 1"))
+Num2 = int(input("Enter the No 2"))
+Num3 = int(input("Enter the No 3"))
+
+sum = Num1 + Num2 + Num3
+sub = Num1 - Num2 - Num3
+Mul = Num1 * Num2 * Num3
+Div = Num1 / Num2 / Num3
+
+print("Sum of 3 no's is :", sum)
+print("Sub of 3 no's is :", sub)
+print("Mul of 3 no's is :", Mul)
+print("Div of 3 no's is :", Div)
---
+++ Nov2024/09112024_Literals & Variables/Lab030_Task1-09112024.py
@@ -0,0 +1,10 @@
+# Task-1 - with 2 no's get Quotient & Remainder
+
+Num1= int(input("Enter Number 1 ->"))
+Num2= int(input("Enter Number 2 ->"))
+
+Remain = Num1%Num2
+Quot = Num1//Num2
+
+print("Reminder-> ", Remain)
+print("Quotient -> ",Quot)
