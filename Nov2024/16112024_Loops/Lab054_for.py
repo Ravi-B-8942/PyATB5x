de6f86b35ec09fb58f324d207789d8a82777b7d6
RaviB <baburavi.bellary90@gmail.com>
2024-11-30 19:28:25 +0530

    19112024 committed

---
+++ Nov2024/16112024_Loops/Lab054_for.py
@@ -0,0 +1,13 @@
+for i in range(0,10):
+    if i == 5:
+        print("Five")
+    else:
+        print(i)
+
+
+print("-------")
+
+for i in range(0,10):
+    print(i)
+    if i == 5:
+        break
---
+++ Nov2024/16112024_Loops/Lab060_Match.py
@@ -0,0 +1,12 @@
+browser_name = input("Enter the browser name\n")
+match browser_name:
+    case "firefox":
+        print("Browser name is firefox")
+    case "Chrome":
+        print("Browser name is chrome")
+    case "Edge":
+        print("Browser name is Edge")
+    case "Internet_Explorer":
+        print("Browser name is Internet_Explorer")
+    case _:
+        print("Browser not found")
